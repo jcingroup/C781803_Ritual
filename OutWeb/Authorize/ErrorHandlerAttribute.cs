784b467445f7efe1c53363f9069da27d35111e3e
好就 莊 <sdec0817@gmail.com>
2018-05-23 14:40:44 +0800

    移除copy專案 WBDB

--- OutWeb/Authorize/ErrorHandlerAttribute.cs
+++ OutWeb/Authorize/ErrorHandlerAttribute.cs
@@ -11,12 +11,12 @@ namespace OutWeb.Authorize
             base.OnException(filterContext);
             Exception exception = filterContext.Exception;
             int logGuId = new System.Random().Next(0, 32767);
-            WBLOGERR Log = new WBLOGERR();
-            Log.ERR_GID = logGuId;
-            Log.ERR_SRC = exception.Source;
-            Log.ERR_SMRY = string.Format("messages：{0} 。 innerException：{1}", exception.Message, exception.InnerException);
-            Log.ERR_DESC = exception.StackTrace;
-            Log.LOG_DTM = DateTime.UtcNow.AddHours(8);
+            //WBLOGERR Log = new WBLOGERR();
+            //Log.ERR_GID = logGuId;
+            //Log.ERR_SRC = exception.Source;
+            //Log.ERR_SMRY = string.Format("messages：{0} 。 innerException：{1}", exception.Message, exception.InnerException);
+            //Log.ERR_DESC = exception.StackTrace;
+            //Log.LOG_DTM = DateTime.UtcNow.AddHours(8);
             //WBDBEntities DB = new WBDBEntities();
             //DB.WBLOGERR.Add(Log);
             //DB.SaveChanges();
